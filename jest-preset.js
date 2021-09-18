20171cf036f9033a2acc23410dba456c31099737
Yuya Saito <hello@studiomohawk.com>
2021-09-18 12:39:29 +0900

    fix: path to package

--- jest-preset.js
+++ jest-preset.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ...require('@frontendweekly/jest-preset-molle/jest-common.js'),
+  ...require('@frontendweekly/molle-jest-preset/jest-common.js'),
   projects: [
     {
       displayName: 'eslint',
@@ -12,14 +12,14 @@ module.exports = {
       testMatch: ['<rootDir>/**/*.(?:p?c|sa)ss'],
     },
     {
-      ...require('@frontendweekly/jest-preset-molle/jest-common.js'),
+      ...require('@frontendweekly/molle-jest-preset/jest-common.js'),
       displayName: 'client',
       setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
       testEnvironment: 'jest-environment-jsdom',
       testMatch: ['**/__client_tests__/**/*.spec.[jt]s?(x)'],
     },
     {
-      ...require('@frontendweekly/jest-preset-molle/jest-common.js'),
+      ...require('@frontendweekly/molle-jest-preset/jest-common.js'),
       displayName: 'server',
       testEnvironment: 'jest-environment-node',
       testMatch: ['**/__tests__/**/*.spec.[jt]s?(x)'],
